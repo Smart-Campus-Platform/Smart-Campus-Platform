545220bffe3cfba6ca3b1f63437f94c6e1aa2a2c
saracanelas <sara.i.canelas@gmail.com>
2026-05-09 14:24:53 +0100

    Initial setup

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('It is working!');
+});
+
+app.listen(3000, () => {
+    console.log('Server in port 3000');
+});
