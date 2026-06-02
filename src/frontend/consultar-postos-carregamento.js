69c67ec74820c37d36463b8f70db73928d18f60d
saracanelas <sara.i.canelas@gmail.com>
2026-06-02 17:51:45 +0100

    Uniformização dos JS carregamento

--- src/frontend/consultar-postos-carregamento.js
+++ src/frontend/consultar-postos-carregamento.js
@@ -1,10 +1,36 @@
 async function carregarPostosUtilizador() {
-    const resposta = await fetch("/api/postos-carregamento");
-    const postos = await resposta.json();
+    try {
+        const resposta = await fetch("/api/postos-carregamento");
+        await verificarResposta(resposta, "Erro ao carregar postos.");
+        const postos = await resposta.json();
 
-    const lista = document.getElementById("lista-postos-carregamento");
-    lista.innerHTML = "";
+        const listaPostos = document.getElementById("lista-postos-carregamento");
+        listaPostos.innerHTML = "";
 
+        const postosPorArea = agruparPostosPorArea(postos);
+
+        Object.keys(postosPorArea).forEach(function(area, indiceArea) {
+            listaPostos.appendChild(criarElementoArea(area, postosPorArea[area], indiceArea));
+        });
+    } catch (erro) {
+        alert(erro.message);
+    }
+}
+
+async function verificarResposta(resposta, mensagemPadrao) {
+    if (resposta.ok) {
+        return;
+    }
+
+    try {
+        const dados = await resposta.json();
+        throw new Error(dados.erro || mensagemPadrao);
+    } catch (erro) {
+        throw new Error(erro.message || mensagemPadrao);
+    }
+}
+
+function agruparPostosPorArea(postos) {
     const postosPorArea = {};
 
     postos.forEach(function(posto) {
@@ -15,34 +41,40 @@ async function carregarPostosUtilizador() {
         postosPorArea[posto.area].push(posto);
     });
 
-    Object.keys(postosPorArea).forEach(function(area, index) {
-        const areaId = "area-postos-" + index;
-        const div = document.createElement("div");
-        div.className = "piso";
-
-        div.innerHTML =
-            '<div class="titulo-piso" onclick="toggleSalas(\'' + areaId + '\', this)">' +
-            '    <span class="seta-piso">&#9656;</span>' +
-            '    <span>' + area + '</span>' +
-            '</div>' +
-            '<div class="salas" id="' + areaId + '">' +
-            postosPorArea[area].map(function(posto) {
-                const disponivel = posto.disponibilidade === true || posto.disponibilidade === 1 || posto.disponibilidade === "1";
-                const classe = disponivel ? "disponivel" : "indisponivel";
-                const texto = disponivel ? "Disponivel" : "Indisponivel";
-
-                return '<div class="lugar">' +
-                    '<span>' + posto.id_posto + '</span>' +
-                    '<span class="indicador ' + classe + '" title="' + texto + '"></span>' +
-                '</div>';
-            }).join("") +
-            '</div>';
-
-        lista.appendChild(div);
-    });
+    return postosPorArea;
 }
+
+function criarElementoArea(area, postos, indiceArea) {
+    const idArea = "area-postos-" + indiceArea;
+    const areaElemento = document.createElement("div");
+    areaElemento.className = "piso";
+
+    areaElemento.innerHTML =
+        "<div class=\"titulo-piso\" onclick=\"toggleSalas('" + idArea + "', this)\">" +
+        "    <span class=\"seta-piso\">&#9656;</span>" +
+        "    <span>" + area + "</span>" +
+        "</div>" +
+        "<div class=\"salas\" id=\"" + idArea + "\">" +
+        postos.map(function(posto) {
+            return criarHtmlPosto(posto);
+        }).join("") +
+        "</div>";
+
+    return areaElemento;
+}
+
+function criarHtmlPosto(posto) {
+    const disponivel = posto.disponibilidade === true || posto.disponibilidade === 1 || posto.disponibilidade === "1";
+    const classeDisponibilidade = disponivel ? "disponivel" : "indisponivel";
+    const textoDisponibilidade = disponivel ? "Disponivel" : "Indisponivel";
+
+    return "<div class=\"lugar\">" +
+        "<span>" + posto.id_posto + "</span>" +
+        "<span class=\"indicador " + classeDisponibilidade + "\" title=\"" + textoDisponibilidade + "\"></span>" +
+    "</div>";
+}
+
 
 document.addEventListener("DOMContentLoaded", function() {
     carregarPostosUtilizador();
-    setInterval(carregarPostosUtilizador, 30000);
 });
